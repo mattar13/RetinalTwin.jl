09bdc0882256817a79166d19fe397d88ef5752b8
mtarchick13@gmail.com <mtarchick13@gmail.com>
2025-04-13 23:29:05 -0700

    updated depc

--- src/DEPC.jl
+++ src/DEPC.jl
@@ -20,21 +20,22 @@ function phototransduction_ode!(du, u, p, t; stim_start = 0.0, stim_end = 1.0, p
 
     #CONSTANTS
     RMAX = 1.0
-    TMAX = 1.0
-    PMAX = 1.0
+
     GMAX = 1.0
 
     #Open parameters
-    (kP1, kP2, kP3, kP4, kP5, kP6,
+    (kR, kP2, kP3, kP4, kP5, kP6,
     l1, h1, JMAX, HMAX,
     τR, τT, τP, τG, τJ, τH) = p
 
-    @. dR = kP1*Stim(t, stim_start, stim_end, photon_flux)*(1-R/RMAX) - R/τR
-    @. dT = kP2*R*(1-T/TMAX)- T/τT
-    @. dP = kP3*T*(1-P/PMAX) - P/τP
+    Φ=Stim(t, stim_start, stim_end, photon_flux)
+    @. dR = kP1*Φ#=*(1-R)=# - R/τR
+    @. dT = kP2*R*(1-T)- T/τT
+    @. dP = kP3*T*(1-P) - P/τP
     @. dG = -kP4*P*(1-G/-GMAX) - G/τG # Non-linear degradation
-    @. dJ = 
-    @. dH = 0.0 #(kP6*H_inf(J, l1, h1)^2*(1-H/ HMAX) - H)/τH #to add this or not *H_inf(J, l1, h1)
+
+    @. dJ = ( kP5*G*(1-J/-JMAX) - J)/τJ
+    @. dH = (kP6*H_inf(J, l1, h1)^2*(1-H/ HMAX) - H)/τH #to add this or not *H_inf(J, l1, h1)
 
     @. dA = (J+H) - A
     return nothing
